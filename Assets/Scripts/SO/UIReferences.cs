becd13e8b4947f4bd75d37f11b074e5ea3b57201
Artem016 <lenovaega2@gmail.com>
2024-10-25 15:06:24 +0500

    /

--- Assets/Scripts/SO/UIReferences.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "UIReferences", menuName = "References/UIReferences")]
-public class UIReferences : ScriptableObject
-{
-    public Transform MonstersUIContainer;
-}
